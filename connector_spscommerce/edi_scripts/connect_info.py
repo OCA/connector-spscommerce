71dd901a55ea40b191ac80423cc34718265db052
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2018-08-20 18:27:26 +0530

    Improve configuration details to more generic

--- connector_spscommerce/edi_scripts/connect_info.py
+++ connector_spscommerce/edi_scripts/connect_info.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 # Set the parameters appropriately before attempting EDI connection
-# OERP Constants
-USERNAME = 'admin'
-PWD = 'admin'
-DBNAME = 'Medico-Master-04032016'
-ERP_WWW = "http://0.0.0.0:8069"
+# Odoo Constants
+USERNAME = '<db username>'
+PWD = '<db password>'
+DBNAME = '<Your Database Name>'
+ERP_WWW = "http://<HOST>:<PORT>" or "ERP Domain"
 EOL_MARKER = '~'
 DEBUG = True
-IN_PATH = '/home/ram/Desktop/spscommercetask/Test/'
+IN_PATH = '/opt/local-addons/connector_spscommerce/test/'
